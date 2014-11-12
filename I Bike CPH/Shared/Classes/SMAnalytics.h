dcc4830cd629ae26654cd483c0ba237018a4512c
Tobias Due Munk <tobias@developmunk.dk>
2014-11-12 02:29:44 +0100

    Prepare for code documentation

--- I Bike CPH/Shared/Classes/SMAnalytics.h
+++ I Bike CPH/Shared/Classes/SMAnalytics.h
@@ -9,10 +9,23 @@
 #import <Foundation/Foundation.h>
 
 /**
- * Google analytics wrapper
+ @class SMAnalytics
+ @abstract Google Analytics wrapper
  */
 @interface SMAnalytics : NSObject
 
+/**
+ Convenience method for tracking event on Google Analytics shared instance.
+ Example usage:
+ @code [SMAnalytics trackEventWithCategory:@"Account" withAction:@"Save" withLabel:@"Data" withValue:0];
+ @endcode
+ @see https://developers.google.com/analytics/devguides/collection/ios for more information.
+ @param category
+ Label for the category for action
+ @param action
+ Label for the action
+ @return YES always, for no particular reason (legacy)
+ */
 + (BOOL)trackEventWithCategory:(NSString*)category withAction:(NSString*)action withLabel:(NSString*)label withValue:(NSInteger)value;
 + (BOOL)trackTimingWithCategory:(NSString*)category withValue:(NSTimeInterval)time withName:(NSString*)name withLabel:(NSString*)label;
 + (BOOL)trackSocial:(NSString*)social withAction:(NSString*)action withTarget:(NSString*)url;
