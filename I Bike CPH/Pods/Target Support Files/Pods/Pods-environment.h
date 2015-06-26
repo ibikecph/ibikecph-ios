07f34c3b7fd1ff12672f7cb8e06b811ac4f207be
Tobias Due Munk <tobias@developmunk.dk>
2015-06-26 10:09:56 +0200

    Update pods

--- I Bike CPH/Pods/Target Support Files/Pods/Pods-environment.h
+++ I Bike CPH/Pods/Target Support Files/Pods/Pods-environment.h
@@ -94,7 +94,7 @@
 #define COCOAPODS_POD_AVAILABLE_TTTAttributedLabel
 #define COCOAPODS_VERSION_MAJOR_TTTAttributedLabel 1
 #define COCOAPODS_VERSION_MINOR_TTTAttributedLabel 13
-#define COCOAPODS_VERSION_PATCH_TTTAttributedLabel 3
+#define COCOAPODS_VERSION_PATCH_TTTAttributedLabel 4
 
 // UIImage-Categories
 #define COCOAPODS_POD_AVAILABLE_UIImage_Categories
