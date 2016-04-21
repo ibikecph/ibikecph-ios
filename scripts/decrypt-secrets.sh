e98bfe563427786c6972c0004fae09c524a3e646
Troels Michael Trebbien <troels@trebbien.codes>
2016-04-21 20:36:52 +0200

    Changed encryption tool from GPG to OpenSSL.

--- scripts/decrypt-secrets.sh
+++ scripts/decrypt-secrets.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Decrypt secret files
-echo $file_enc_password | gpg --passphrase-fd 0 secrets/ios/certs/distribution.cer.gpg
-echo $file_enc_password | gpg --passphrase-fd 0 secrets/ios/certs/distribution.p12.gpg
-echo $file_enc_password | gpg --passphrase-fd 0 secrets/ios/profile/cykelplanen_ad_hoc.mobileprovision.gpg
-echo $file_enc_password | gpg --passphrase-fd 0 secrets/ios/profile/ibikecph_ad_hoc.mobileprovision.gpg
-echo $file_enc_password | gpg --passphrase-fd 0 secrets/ios/shared/smroute_settings_private.plist.gpg
-echo $file_enc_password | gpg --passphrase-fd 0 secrets/ios/ibikecph/smroute_settings_app_private.plist.gpg
-echo $file_enc_password | gpg --passphrase-fd 0 secrets/ios/cykelplanen/smroute_settings_app_private.plist.gpg
+openssl aes-256-cbc -k "$file_enc_password" -in secrets/ios/certs/distribution.cer.enc -out secrets/ios/certs/distribution.cer -d
+openssl aes-256-cbc -k "$file_enc_password" -in secrets/ios/certs/distribution.p12.enc -out secrets/ios/certs/distribution.p12 -d
+openssl aes-256-cbc -k "$file_enc_password" -in secrets/ios/profile/ibikecph_ad_hoc.mobileprovision.enc -out secrets/ios/profile/ibikecph_ad_hoc.mobileprovision -d
+openssl aes-256-cbc -k "$file_enc_password" -in secrets/ios/profile/cykelplanen_ad_hoc.mobileprovision.enc -out secrets/ios/profile/cykelplanen_ad_hoc.mobileprovision -d
+openssl aes-256-cbc -k "$file_enc_password" -in secrets/ios/shared/smroute_settings_private.plist.enc -out secrets/ios/shared/smroute_settings_private.plist -d
+openssl aes-256-cbc -k "$file_enc_password" -in secrets/ios/ibikecph/smroute_settings_app_private.plist.enc -out secrets/ios/ibikecph/smroute_settings_app_private.plist -d
+openssl aes-256-cbc -k "$file_enc_password" -in secrets/ios/cykelplanen/smroute_settings_app_private.plist.enc -out secrets/ios/cykelplanen/smroute_settings_app_private.plist -d
