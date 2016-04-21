60127970daeda516edf5a7a95ea61294b353d2b5
Troels Michael Trebbien <troels@trebbien.codes>
2016-04-21 18:29:10 +0200

    Updated Travis scripts.

--- scripts/testflight.sh
+++ scripts/testflight.sh
@@ -3,32 +3,35 @@ if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
   echo "This is a pull request. No deployment will be done."
   exit 0
 fi
-if [[ "$TRAVIS_BRANCH" != "master" ]]; then
-  echo "Testing on a branch other than master. No deployment will be done."
+if [[ "$TRAVIS_BRANCH" != "develop" ]]; then
+  echo "Testing on a branch other than develop. No deployment will be done."
   exit 0
 fi
 
-# Thanks @djacobs https://gist.github.com/djacobs/2411095
-
-PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_UUID.mobileprovision"
+PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$prov_profile_name.mobileprovision"
 RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
 OUTPUTDIR="$PWD/build/Release-iphoneos"
 
 echo "********************"
 echo "*     Signing      *"
 echo "********************"
-xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APPNAME.app" -o "$OUTPUTDIR/$APPNAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
+xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$app_name.app" -o "$OUTPUTDIR/$app_name.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
+
+
+
+
+
 
 RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER\nUploaded: $RELEASE_DATE"
 
-zip -r -9 "$OUTPUTDIR/$APPNAME.app.dSYM.zip" "$OUTPUTDIR/$APPNAME.app.dSYM"
+zip -r -9 "$OUTPUTDIR/$app_name.app.dSYM.zip" "$OUTPUTDIR/$app_name.app.dSYM"
 
 echo "********************"
 echo "*    Uploading     *"
 echo "********************"
 curl http://testflightapp.com/api/builds.json \
-  -F file="@$OUTPUTDIR/$APPNAME.ipa" \
-  -F dsym="@$OUTPUTDIR/$APPNAME.app.dSYM.zip" \
+  -F file="@$OUTPUTDIR/$app_name.ipa" \
+  -F dsym="@$OUTPUTDIR/$app_name.app.dSYM.zip" \
   -F api_token="$API_TOKEN" \
   -F team_token="$TEAM_TOKEN" \
   -F distribution_lists='Internal' \
