5ae38b109ea1c4621c109319af474a341891adc0
Troels Michael Trebbien <troels@trebbien.codes>
2016-04-23 17:52:25 +0200

    Changed branch condition in after script.

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -3,7 +3,7 @@ if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
   echo "This is a pull request. No deployment will be done."
   exit 0
 fi
-if [[ "$TRAVIS_BRANCH" != "develop" ]]; then
+if [[ "$TRAVIS_BRANCH" != "ci-test" ]]; then
   echo "Testing on a branch other than develop. No deployment will be done."
   exit 0
 fi
