5adef873eeec2d0df1b75a2d33409dc659c24bcc
Troels Michael Trebbien <troels@trebbien.codes>
2016-04-24 14:35:22 +0200

    Attempt to use gym for building and signing.

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -13,8 +13,7 @@ RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
 OUTPUTDIR="$PWD/build/Release-iphoneos"
 
 echo "********************"
-echo "*     Signing      *"
+echo "*     Uploading    *"
 echo "********************"
-xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$app_name.app" -o "$OUTPUTDIR/$app_name.ipa" -sign "$developer_name" -embed "$PROVISIONING_PROFILE"
 
 deliver --ipa "$OUTPUTDIR/$app_name.ipa"
