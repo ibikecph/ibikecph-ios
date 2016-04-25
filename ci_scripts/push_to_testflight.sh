8185e379474ed64451e9985323315552b80c15d8
Troels Michael Trebbien <troels@trebbien.codes>
2016-04-25 11:20:35 +0200

    Changed CI scripts.

--- ci_scripts/push_to_testflight.sh
+++ ci_scripts/push_to_testflight.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
-if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
-  echo "This is a pull request. No deployment will be done."
-  exit 0
-fi
-if [[ "$TRAVIS_BRANCH" != "ci-testflight" ]]; then
-  echo "Building on a branch other than ci-testflight. No deployment will be done."
-  exit 0
-fi
+if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "ci-testflight" ]]; then
+  echo "********************"
+  echo "*     Uploading    *"
+  echo "********************"
 
-echo "********************"
-echo "*     Uploading    *"
-echo "********************"
-
-deliver -a "$BUNDLE_IDENTIFIER_0" -i "$OUTPUTDIR/$APP_NAME_0.ipa" -m "$METADATA_PATH_0"
-deliver -a "$BUNDLE_IDENTIFIER_1" -i "$OUTPUTDIR/$APP_NAME_1.ipa" -m "$METADATA_PATH_1"
+  deliver -a "$BUNDLE_IDENTIFIER_0" -i "$OUTPUTDIR/$APP_NAME_0.ipa" -m "$METADATA_PATH_0"
+  deliver -a "$BUNDLE_IDENTIFIER_1" -i "$OUTPUTDIR/$APP_NAME_1.ipa" -m "$METADATA_PATH_1"
+else
+  echo "Building on a branch other than ci-testflight or from a pull request. No deployment will be done."
+fi
