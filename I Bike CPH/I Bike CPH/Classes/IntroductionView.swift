8c99c6b42ff1745e0ce3d65300214c1531c35320
Troels Michael Trebbien <troels@trebbien.codes>
2016-05-23 12:47:51 +0200

    Started rewriting greenest route introduction view.

--- I Bike CPH/I Bike CPH/Classes/IntroductionView.swift
+++ I Bike CPH/I Bike CPH/Classes/IntroductionView.swift
@@ -14,84 +14,67 @@ class IntroductionView: EAIntroView {
     override init(frame: CGRect) {
         // Create pages
         var pages = [EAIntroPage]()
-        let bodyTexts = "introduction_greenest_route_body_ibc".localized.componentsSeparatedByString("\n")
         
-        var titleViewPositionY: CGFloat
-        var titleIconPositionY: CGFloat
-        var titlePositionY: CGFloat
-        var descPositionY: CGFloat
-        var descSideMargin: CGFloat
+        let introPage = EAIntroPage()
+        let introPageView = UIView.init(frame: frame)
+        introPageView.backgroundColor = UIColor.clearColor()
         
-        // Make up for poor customization options in EAIntroView
-        print(frame.size.height)
-        switch frame.size.height {
-        case 0..<481:
-            // iPhone 4S
-            titleViewPositionY = 50
-            titleIconPositionY = 250
-            titlePositionY = 250
-            descPositionY = titlePositionY + 50
-            descSideMargin = 30
-        case 481..<668:
-            // iPhone 5, 5S, 6, 6S
-            titleViewPositionY = 100
-            titleIconPositionY = 300
-            titlePositionY = 250
-            descPositionY = titlePositionY + 50
-            descSideMargin = 50
-        default:
-            // Any other device (like iPhone 6, 6S)
-            titleViewPositionY = 150
-            titleIconPositionY = 350
-            titlePositionY = 250
-            descPositionY = titlePositionY + 50
-            descSideMargin = 70
-        }
+        let headerImage = UIImage(named: "IntroductionGreenestRouteHeader")
+        let headerImageViewFrame = CGRect(x: 0, y: 30, width: frame.size.width, height: headerImage?.size.height ?? 0)
+        let headerImageView = UIImageView.init(frame: headerImageViewFrame)
+        headerImageView.clipsToBounds = true
+        headerImageView.image = headerImage
+        headerImageView.contentMode = .Center
+        introPageView.addSubview(headerImageView)
+        
+        let titleLabelFrame = CGRect(x: 0,
+                                     y: headerImageViewFrame.origin.y + headerImageViewFrame.size.height + 10,
+                                     width: frame.size.width,
+                                     height: 35)
+        let titleLabel = UILabel.init(frame: titleLabelFrame)
+        titleLabel.text = "introduction_greenest_route_header_ibc".localized
+        titleLabel.textAlignment = .Center
+        titleLabel.textColor = UIColor.whiteColor()
+        titleLabel.font = UIFont.boldSystemFontOfSize(26)
+        introPageView.addSubview(titleLabel)
+        
+        let footerButtonFrame = CGRect(x: 0,
+                                       y: frame.size.height-60,
+                                       width: frame.size.width,
+                                       height: 60)
+        let footerButton = UIButton.init(frame: footerButtonFrame)
+        footerButton.titleLabel?.font = UIFont.systemFontOfSize(16)
+        footerButton.titleLabel?.textAlignment = .Center
+        footerButton.titleLabel?.textColor = Styler.tintColor()
+        footerButton.backgroundColor = UIColor.whiteColor()
+        footerButton.titleLabel?.text = "OK".localized
+        introPageView.addSubview(footerButton)
+        
+        let bodyTextViewFrame = CGRect(x: 20,
+                                       y: titleLabelFrame.origin.y + titleLabelFrame.size.height + 20,
+                                       width: frame.size.width-20,
+                                       height: frame.size.height - (titleLabelFrame.origin.y + titleLabelFrame.size.height) - footerButtonFrame.size.height - 30)
+        let bodyTextView = UITextView.init(frame: bodyTextViewFrame)
+        bodyTextView.textColor = UIColor.whiteColor()
+        bodyTextView.text = "introduction_greenest_route_body_ibc".localized
+        bodyTextView.textAlignment = .Left
+        bodyTextView.editable = false
+        bodyTextView.selectable = false
+        bodyTextView.showsHorizontalScrollIndicator = false
+        bodyTextView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 10, right: 0)
+        bodyTextView.backgroundColor = UIColor.clearColor()
+        bodyTextView.font = UIFont.preferredFontForTextStyle(UIFontTextStyleBody)
+        introPageView.addSubview(bodyTextView)
+        
+        introPage.customView = introPageView
+        pages.append(introPage)
         
         // Instantiate this here for alignment reasons
-        let leafIconImageView = UIImageView.init(image: UIImage(named: "Green"))
-        
-        let pageOne = EAIntroPage()
-        pageOne.title = "introduction_greenest_route_header_ibc".localized
-        pageOne.titleIconView = UIView(frame: leafIconImageView.frame)
-        pageOne.titleIconPositionY = titleIconPositionY
-        pageOne.titlePositionY = titlePositionY
-        pageOne.descPositionY = descPositionY
-        pageOne.desc = bodyTexts.count > 0 ? bodyTexts[0] : ""
-        pageOne.descSideMargin = descSideMargin
-        pages.append(pageOne)
-        
-        let pageTwo = EAIntroPage()
-        pageTwo.title = " "
-        pageTwo.titleIconView = UIView(frame: leafIconImageView.frame)
-        pageTwo.titleIconPositionY = titleIconPositionY
-        pageTwo.titlePositionY = titlePositionY
-        pageTwo.descPositionY = descPositionY
-        pageTwo.desc = bodyTexts.count > 1 ? bodyTexts[1] : ""
-        pageTwo.descSideMargin = descSideMargin
-        pages.append(pageTwo)
-        
-        let pageThree = EAIntroPage()
-        pageThree.title = "introduction_greenest_route_footer_ibc".localized
-        pageThree.titleIconView = leafIconImageView
-        pageThree.titleIconPositionY = titleIconPositionY
-        pageThree.titlePositionY = titlePositionY
-        pageThree.descPositionY = descPositionY
-        pageThree.desc = bodyTexts.count > 2 ? bodyTexts[2] : ""
-        pageThree.descSideMargin = descSideMargin
-        pages.append(pageThree)
+//        let leafIconImageView = UIImageView.init(image: UIImage(named: "Green"))
         
         super.init(frame: frame, andPages: pages)
         
-        let titleView = UIImageView.init(image: UIImage(named: "IntroductionGreenestRouteHeader"))
-        titleView.layer.cornerRadius = 5.0
-        titleView.clipsToBounds = true
-        titleView.layer.borderWidth = 1.0
-        titleView.layer.borderColor = UIColor.whiteColor().CGColor
-        
-        self.titleView = titleView
-        self.titleViewY = titleViewPositionY
-        self.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.8)
+        self.backgroundColor = Styler.tintColor()
     }
     
     required init!(coder: NSCoder) {
