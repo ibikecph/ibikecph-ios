f68b9748c5c77905f4c82ed8918c3ceafb400f5f
Babak Feiz <bfe@netcompany.com>
2018-05-11 16:12:57 +0200

    Fixes in JSON-related logic

--- I Bike CPH/Shared/Classes/RoutingEngine/Models/FavoriteItem.swift
+++ I Bike CPH/Shared/Classes/RoutingEngine/Models/FavoriteItem.swift
@@ -131,10 +131,10 @@ import SwiftyJSON
         
         // Street, name, address, city, zip. Fallback to full address string
         let parsedAddressItem = SMAddressParser.parseAddress(address)
-        number = json["husnr"].string ?? parsedAddressItem?.number
-        city = json["by"].string ?? parsedAddressItem?.city
-        zip = json["postnummer"].string ?? parsedAddressItem?.zip
-        street = json["vej"].string ?? parsedAddressItem?.street
+        number = json["husnr"].string ?? (parsedAddressItem?.number)!
+        city = json["by"].string ?? (parsedAddressItem?.city)!
+        zip = json["postnummer"].string ?? (parsedAddressItem?.zip)!
+        street = json["vej"].string ?? (parsedAddressItem?.street)!
         
         if let data = plistDictionary["startDate"] as? Data {
             startDate = NSKeyedUnarchiver.unarchiveObject(with: data) as? Date
